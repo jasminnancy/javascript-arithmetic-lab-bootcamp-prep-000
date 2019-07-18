d54bb3ef9675c653022dfbe02eef96784038654f
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-18 23:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ function add(a, b) {return a + b}
 function subtract(a, b) {return a - b}
 function multiply(a, b) {return a * b}
 function divide(a, b) {return a / b}
-var number = a
-function inc(n) {return number++}
-function dec(n) {return number--}
+function inc(n) {return n++}
+function dec(n) {return n--}
